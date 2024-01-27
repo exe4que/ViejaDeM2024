7c7462ad696ab46675b547dd50df6ab4ba18b88c
exe4que <exe4que@gmail.com>
2024-01-27 12:50:52 -0300

    outline agregado

--- Scripts/Vieja.gd
+++ Scripts/Vieja.gd
@@ -2,9 +2,12 @@ class_name Vieja
 extends Node2D
 
 @export var speed: float = 5
+@export var myMaterial: ShaderMaterial
+
 
 var moveVector = Vector2(0, 0)
 var position3d = Vector3(0, 0, 0)
+
 
 func _ready():
 	position3d = Vector3(position.x, 0, position.y)
@@ -30,7 +33,9 @@ func _physics_process(delta):
 	position3d.x = clampf(position3d.x, hLimits.x, hLimits.y)
 	global_position = Vector2(position3d.x, position3d.y + position3d.z)
 	pass
-	
+
+
+var highlighted = false;
 func _handle_inputs():
 	moveVector = Vector2(0,0)
 	if Input.is_action_pressed("right"):
@@ -42,3 +47,12 @@ func _handle_inputs():
 	if Input.is_action_pressed("down"):
 		moveVector.y += 1
 	moveVector = moveVector.normalized()
+		
+	if Input.is_action_just_pressed("main_action"):
+		highlighted = !highlighted
+		set_highlight(highlighted)
+		
+func set_highlight(highlighted):
+	var color = myMaterial.get_shader_parameter("line_color")
+	color.a = 1 if highlighted else 0
+	myMaterial.set_shader_parameter("line_color", color)
