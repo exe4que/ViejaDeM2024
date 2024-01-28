ff8042428375169c1018af1b763fc28fab1c62ab
NicolasBroglia <107767100+NicolasBroglia@users.noreply.github.com>
2024-01-28 08:12:01 -0300

    errores

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -7,3 +7,5 @@ func _on_start_button_button_down():
 	$VBoxContainer/ControlsButton.hide()
 	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://Scenes/transition_screen.tscn")
+	
+	
