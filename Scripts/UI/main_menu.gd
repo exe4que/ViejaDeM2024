84ee6d483af8db1bcd65e7668517e4a37bd47830
NicolasBroglia <107767100+NicolasBroglia@users.noreply.github.com>
2024-01-28 13:10:01 -0300

    Added Cinematic and changed main Scene

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,11 +1,9 @@
 extends Control
 
 func _on_start_button_button_down():
-	$ColorRect2/AnimationPlayer.play("main_menu_fade_out")
+	$AnimationPlayer.play("main_menu_fade_out")
 	$VBoxContainer/StartButton.hide()
 	$VBoxContainer/QuitButton.hide()
 	$VBoxContainer/ControlsButton.hide()
 	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://Scenes/transition_screen.tscn")
-	
-	
