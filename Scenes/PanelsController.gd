997e44d7294db11f0819fc25babadfdacf5fa54e
exe4que <exe4que@gmail.com>
2024-01-28 07:30:33 -0300

    pantallas de won y lost

---
+++ Scenes/PanelsController.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@export var levelWonPanel: CanvasModulate
+@export var levelLostPanel: CanvasModulate
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GlobalManager.level_lost.connect(_onLevelLost)
+	GlobalManager.level_won.connect(_onLevelWon)
+
+func _onLevelWon():
+	levelWonPanel.show()
+	get_tree().create_tween().tween_property(levelWonPanel, "color", Color.WHITE, 1)
+	
+func _onLevelLost():
+	levelLostPanel.show()
+	get_tree().create_tween().tween_property(levelLostPanel, "color", Color.WHITE, 1)
+
--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var verticalLimits: Vector2 = Vector2(380, 700)
 @export var horizontallLimits: Vector2 = Vector2(0, 1152)
 
+signal level_won()
+signal level_lost()
+
 #3 * 60 = 3min
 var gameDuration = 3 * 60
 var running = true
@@ -22,8 +25,10 @@ func _process(delta):
 
 func won():
 	running = false
+	level_won.emit()
 	pass
 
 func lost():
 	running = false
+	level_lost.emit()
 	pass
